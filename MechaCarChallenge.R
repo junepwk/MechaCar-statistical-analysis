c91dadba7b9c02cb7ac64a6cb8e9231e4a13c917
junepwk <82183218+junepwk@users.noreply.github.com>
2021-08-08 21:07:40 -0400

    multiple linear regression RScript update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,1 +1,11 @@
+library(tidyverse)
+library(dplyr)
 
+# Import and read in MechaCar_mpg.csv as a dataframe
+mpg_data <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+# Multiple linear regression model to predict mpg
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mpg_data) 
+
+# Summary statistic to determine p-value and r-squared value
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mpg_data))
