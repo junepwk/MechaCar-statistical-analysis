7977aa77433f7371f87957e936009d20e6b1a696
junepwk <82183218+junepwk@users.noreply.github.com>
2021-08-08 23:00:27 -0400

    test if the PSI is statistically different from population mean

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -18,3 +18,17 @@ total_summary <- coil_data %>% summarize(Mean=mean(PSI), Median=median(PSI), Var
 
 # Summary statistics grouped by the manufacturing lot
 lot_summary <- coil_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+# Using t-Test to determine if the PSI across all manufacturing lots is statistically
+# different from the population mean of 1,500 pounds per square inch
+t.test(coil_data$PSI, mu=1500)
+
+# Test for each individual lot
+lot1_data <- subset(coil_data, Manufacturing_Lot == "Lot1")
+t.test(lot1_data$PSI, mu=1500)
+
+lot2_data <- subset(coil_data, Manufacturing_Lot == "Lot2")
+t.test(lot2_data$PSI, mu=1500)
+
+lot3_data <- subset(coil_data, Manufacturing_Lot == "Lot3")
+t.test(lot3_data$PSI, mu=1500)
