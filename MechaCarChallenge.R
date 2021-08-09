4e2ee3b5879f3a9f60c5a11a8a61579d8ca87389
junepwk <82183218+junepwk@users.noreply.github.com>
2021-08-08 21:48:54 -0400

    summary statistics on suspension coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -9,3 +9,12 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 # Summary statistic to determine p-value and r-squared value
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mpg_data))
+
+# Import and read in Suspension_Coil.csv
+coil_data <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+# Summary statistics table for PSI column 
+total_summary <- coil_data %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+# Summary statistics grouped by the manufacturing lot
+lot_summary <- coil_data %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
